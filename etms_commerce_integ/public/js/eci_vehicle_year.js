742071d5d1486ea098370d96566c9703b3423a28
Imad Abdou <igentle.appletec@gmail.com>
2022-05-18 10:55:42 +0100

    product condition CF

---
+++ etms_commerce_integ/public/js/eci_vehicle_year.js
@@ -0,0 +1,10 @@
+// frappe.ui.form.on("ECI Vehicle Year", {
+//     refresh: function(frm) {
+//         var parent_compat = frm.doc.model;
+//         if(!frm.doc.__unsaved == 1) {
+//             frm.add_custom_button("Create Year Compat", function() {
+//                 frappe.new_doc("ECI Vehicle Year", {parent_compat: parent_compat});
+//             })
+//         }
+//     }
+// });
