cd2bfe89be51b71621208da19c6efecf71c3219d
Imad Abdou <igentle.appletec@gmail.com>
2022-06-01 22:04:17 +0100

    feat: Custom ads system

--- etms_commerce_integ/ads.py
+++ etms_commerce_integ/ads.py
@@ -3,8 +3,18 @@ from etms_commerce_integ.auth import eci_verify_request
 from etms_commerce_integ.auth import eci_log_error
 
 
-@frappe.whitelist(allow_guest=False, methods=["GET"])
+@frappe.whitelist(allow_guest=False, methods=["POST"])
 @eci_verify_request
-def get_ads():
+def get_next_ad():
+    display_order = frappe.form_dict['display_order']
 
-    pass
+    ads = frappe.get_all("ECI Advertisements",
+                         fields=["ad_image", "display_order"],
+                         filters={"enabled": True})
+
+    next_ad = list(filter(lambda ad: ad.display_order == display_order, ads))
+
+    if next_ad:
+        return {"message": "next_ad", "data": next_ad[0]}
+    else:
+        return {"message": "reset_order"}
