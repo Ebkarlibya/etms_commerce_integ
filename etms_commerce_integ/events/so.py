42bb46315c06c346513dbe6628fcce219b76a5dc
Imad Abdou <igentle.appletec@gmail.com>
2025-03-04 08:49:02 +0000

    disable notify

--- etms_commerce_integ/events/so.py
+++ etms_commerce_integ/events/so.py
@@ -3,13 +3,13 @@ from etms_commerce_integ.notify import ets_send_notification
 
 def on_update(doc, method):
     print("new status")
-    if doc.is_eci_order:
-        # send notification to user via one signal
-        ets_send_notification(
-            message=f"تم تحديث طلبك رقم {doc.name}",
-            username=doc.customer,
-            data={
-                "type": "order_status",
-                "order_id": doc.name
-            }
-        )
+    # if doc.is_eci_order:
+    #     # send notification to user via one signal
+    #     ets_send_notification(
+    #         message=f"تم تحديث طلبك رقم {doc.name}",
+    #         username=doc.customer,
+    #         data={
+    #             "type": "order_status",
+    #             "order_id": doc.name
+    #         }
+    #     )
